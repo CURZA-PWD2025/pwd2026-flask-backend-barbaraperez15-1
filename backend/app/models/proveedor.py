42d11b8aa8c524a6bb71dfa60aa527723554148c
Barbara Perez <barbyperez2025@gmail.com>
2026-04-17 00:13:53 -0300

    Create proveedor.py

---
+++ backend/app/models/proveedor.py
@@ -0,0 +1,11 @@
+from app.models import db
+
+class Proveedor(db.model):
+    __tablename__="categorias"
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False, unique=True)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    contacto = db.Column(db.String(255), nullable=True)
+    telefono = db.Column(db.String(20), nullable=True)
+    email = db.Column(db.String(255), nullable=True)
